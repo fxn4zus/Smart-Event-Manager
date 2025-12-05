275568f3a9561758e2ede787be9bbfab6dc8a45c
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-12-05 18:31:33 +0545

    refactor: restructure server routes and improve health check endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).send("Server is healthy");
 });
 
 app.listen(PORT, () => {
