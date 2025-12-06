3f136bb8e5982d8ddc4f4c832842740af170d945
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-12-06 21:48:45 +0545

    feat: add cookie-parser middleware for handling cookies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
